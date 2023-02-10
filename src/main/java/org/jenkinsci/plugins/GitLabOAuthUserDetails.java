b8e5404eb11b22fbe8c00647038f3ed89f849c20
Mark Waite <mark.earl.waite@gmail.com>
2023-02-10 13:23:50 -0700

    Undo GitLabOAuthUserDetails unexpected change

--- src/main/java/org/jenkinsci/plugins/GitLabOAuthUserDetails.java
+++ src/main/java/org/jenkinsci/plugins/GitLabOAuthUserDetails.java
@@ -1,12 +1,7 @@
-/**
- *
- */
-
 package org.jenkinsci.plugins;
 
 import org.acegisecurity.GrantedAuthority;
 import org.acegisecurity.userdetails.User;
-
 
 /**
  * @author Mike
@@ -17,7 +12,7 @@ public class GitLabOAuthUserDetails extends User {
     private static final long serialVersionUID = 1709511212188366292L;
 
     public GitLabOAuthUserDetails(org.gitlab4j.api.models.User user, GrantedAuthority[] authorities) {
-        super(user.getUsername(), user.getEmail(), true, true, true, true, authorities);
+        super(user.getUsername(), "", true, true, true, true, authorities);
     }
 
 }
